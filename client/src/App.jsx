45ead0a74339bf30ac729859df3cf0f15a9fcd36
arvindsubbu <arvindsonu14@gmail.com>
2025-09-28 21:36:50 +0530

    add ProtectedRoute import to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,10 +8,10 @@ import Orders from "./pages/Orders";
 import ServiceResults from "./pages/ServiceResults";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import ProtectedRoute from './Components/ProtectedRoute'
 import Consumer from "./pages/Consumer";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
